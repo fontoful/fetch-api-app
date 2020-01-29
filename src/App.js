15b0e50a2aa24e47e4a858b3d9e75c0eb8c344ca
Hector <fonto.serrano@gmail.com>
2020-01-28 22:41:50 -0600

    added API key

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from "react"
+import React from "react";
+
+const API_KEY = "15067697-a347138b0dcbd88a15f281e67";
 
 const App = () => (
-  <div>
-    <span role="img" aria-label="emoji">What up 😎</span>
-  </div>
-)
+	<div>
+		<span role="img" aria-label="emoji">
+			What up 😎
+		</span>
+	</div>
+);
 
-export default App
+export default App;
